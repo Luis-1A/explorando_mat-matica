c8053a7d3dcd93b04270519e27e9df69629f981b
Luis Fernando santos <142061648+feito-pelo@users.noreply.github.com>
2024-02-24 20:45:52 -0300

    Add files via upload
    Signed-off-by: Luis Fernando santos  <142061648+feito-pelo@users.noreply.github.com>

---
+++ estilo.js
@@ -0,0 +1,13 @@
+const buttonsComponent = document.querySelector('.buttons');
+const buttonsToggle = document.querySelector('.buttons__toggle');
+
+buttonsToggle.addEventListener('click', toggleButtons);
+
+function toggleButtons() {
+	buttonsToggle.classList.toggle('buttons__toggle--active');
+	buttonsComponent.classList.toggle('buttons--active');
+	document.activeElement.blur();
+}
+
+//tudo daqui pra baixo pode apaga
+  
