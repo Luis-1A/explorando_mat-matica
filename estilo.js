663034fbececa7e7b36b32f69a5a65aadeb66046
Luis Fernando santos <142061648+feito-pelo@users.noreply.github.com>
2024-02-29 11:11:24 -0300

    Update estilo.js
    Signed-off-by: Luis Fernando santos  <142061648+feito-pelo@users.noreply.github.com>

--- estilo.js
+++ estilo.js
@@ -10,4 +10,24 @@ function toggleButtons() {
 }
 
 //tudo daqui pra baixo pode apaga
-  
+
+
+const toggleButton = document.querySelector('.buttons__toggle');
+const allButton = document.querySelector('.allbtns');
+let clicked = false;
+
+toggleButton.addEventListener('click', function() {
+    if (!clicked) {
+        allButton.style.display = 'block'; 
+        setTimeout(function() {
+            toggleButton.setAttribute('data-clicked', 'true'); 
+        }, 1000);
+        clicked = true;
+    } else {
+        setTimeout(function() {
+            allButton.style.display = 'none'; 
+            toggleButton.removeAttribute('data-clicked'); 
+            clicked = false;
+        }, 4000);
+    }
+});
