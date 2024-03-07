a802772eff92b3020e8cbef3035aef22c8f2e964
Luis Fernando santos <142061648+feito-pelo@users.noreply.github.com>
2024-03-07 11:00:41 -0300

    nao esqueca de semple arrumar os botoes

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-const text = "Por favor, acesse pelo dispositivo móvel";
-const textElement = document.querySelector('.animation-text');
+const buttonsComponent = document.querySelector('.buttons');
+const buttonsToggle = document.querySelector('.buttons__toggle');
+const allButton = document.querySelector('.allbtns');
 
-let index = 0;
+buttonsToggle.addEventListener('click', toggleButtons);
 
-function showText() {
-  if (index < text.length) {
-    textElement.textContent += text[index];
-    index++;
-    setTimeout(showText, 200);
-  } else {
-    textElement.style.color = 'red'; 
-  }
+function toggleButtons() {
+    buttonsToggle.classList.toggle('buttons__toggle--active');
+    buttonsComponent.classList.toggle('buttons--active');
+    document.activeElement.blur();
+
+    const currentPosition = allButton.getAttribute('data-position');
+
+    if (currentPosition === 'original' || !currentPosition) {
+        moveAllButton('90%', '51%');
+        allButton.setAttribute('data-position', 'moved');
+    } else {
+        setTimeout(function () {
+            moveAllButton('75%', '50%');
+            allButton.setAttribute('data-position', 'original');
+
+            setTimeout(function () {
+                allButton.style.zIndex = '-1';
+                allButton.style.opacity = '0';
+            }, 500);
+        }, 920);
+    }
 }
 
-showText();
+function moveAllButton(top, left) {
+    allButton.style.zIndex = '1';
+    allButton.style.opacity = '1';
+    allButton.style.transition = 'top 0.5s, left 0.5s';
+    allButton.style.top = top;
+    allButton.style.left = left;
+
+  
+    buttonsToggle.style.top = '75%';
+    buttonsToggle.style.left = '50%';
+
+    setTimeout(() => {
+        allButton.style.transition = ''; 
+    }, 500);
+}
